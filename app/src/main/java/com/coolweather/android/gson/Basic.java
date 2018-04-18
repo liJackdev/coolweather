8a82d448d0b221f5ab5ac99725eaa8d0c72f6f1c
Tony <tony@gmail.com>
2018-04-18 17:24:35 +0800

    加入显示天气信息的功能

--- app/src/main/java/com/coolweather/android/gson/Basic.java
+++ app/src/main/java/com/coolweather/android/gson/Basic.java
@@ -11,11 +11,12 @@ public class Basic {
     public String cityName;
 
     @SerializedName("id")
+    public String weatherId;
     public Update update;
     public  class Update {
 
         @SerializedName("loc")
-        public String updateTiem;
+        public String updateTime;
     }
 }
 
